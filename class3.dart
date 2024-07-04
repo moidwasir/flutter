b82f157acf37a67a61604847cc657a1f3a5c0131
moidwasir <moeed.mehmood@gmail.com>
2024-07-04 17:49:29 +0500

    test class thee

---
+++ class3.dart
@@ -0,0 +1,24 @@
+void main() {
+  String name = "call me moid";
+  print(name);
+
+  int age = 35;
+  print(age);
+
+  double price = 25.6;
+  print(price);
+
+  num total = age + price;
+
+  print(total);
+
+  bool result = price < age;
+
+  print(result);
+
+  if (price <= age) {
+    print("this is ture");
+  } else {
+    print("this is not ture");
+  }
+}
